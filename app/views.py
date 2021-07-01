7879d73de8d2cfa7703555d1f2d38d344223c478
Aroshi Ghosh <yoshighosh@gmail.com>
2021-07-01 15:13:50 -0700

    add js files

--- app/views.py
+++ app/views.py
@@ -1,12 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = False
 
 @app.route('/')
-
-def hello_world():
-  return 'Hello World'
-
+def home():
+   return render_template('home.html')
 if __name__ == '__main__':
-  app.run()
+   app.run()
